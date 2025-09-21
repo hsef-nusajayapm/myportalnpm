fd75fdadd12afedc717388665c7377509dbedce6
hsef NusajayaPM <hsef.nusajayapm@gmail.com>
2025-09-21 10:47:59 +0800

    Tally Adjust 2

--- app/home/page.js
+++ app/home/page.js
@@ -9,6 +9,15 @@ export default function HomePage() {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [activeMenu, setActiveMenu] = useState("dashboard");
 
+// 🔹 Panggil Tally.loadEmbeds() setiap kali ganti menu
+  useEffect(() => {
+    if (activeMenu === "formPortal" && typeof window !== "undefined") {
+      if (window.Tally && typeof window.Tally.loadEmbeds === "function") {
+        window.Tally.loadEmbeds();
+      }
+    }
+  }, [activeMenu]);
+
   return (
     <div className="flex flex-grow">
       <Sidebar styles={styles} isCollapsed={isCollapsed} toggleSidebar={() => setIsCollapsed(!isCollapsed)} activeMenu={activeMenu} setActiveMenu={setActiveMenu} />
@@ -18,7 +27,7 @@ export default function HomePage() {
           {activeMenu === "dashboard" && <div className="bg-white rounded p-4 shadow min-h-screen">📊 Grafik dashboard</div>}
           {activeMenu === "formPortal" && (
             <div className="bg-white rounded p-4 shadow min-h-screen">
-              <div className="container mx-auto max-w-4xl">
+              <div className="container mx-auto max-w-3xl">
                   <iframe
                     src="https://tally.so/embed/mOqMG7?alignLeft=1&transparentBackground=1&dynamicHeight=1"
                     className="w-full"
